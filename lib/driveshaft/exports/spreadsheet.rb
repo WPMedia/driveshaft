a86e0dd0bf2ab131f71d70f6698459d821d85fc6
Aaron Williams <aaron.colby.williams@gmail.com>
2017-12-22 12:37:18 -0500

    Pretty print spreadsheet JSON. For #9

--- lib/driveshaft/exports/spreadsheet.rb
+++ lib/driveshaft/exports/spreadsheet.rb
@@ -28,7 +28,7 @@ module Driveshaft
       end
 
       return {
-        body: JSON.dump(data),
+        body: JSON.pretty_generate(data),
         content_type: 'application/json; charset=utf-8'
       }
     end
